94de332073676089ec5e4d723a240056aeb5cc01
Stacy Wilkerson <wil15061@byui.edu>
2017-10-11 22:19:25 -0600

    Case Study 4 and Task 9

--- M335_Tasks/Case_Study_05/Class_Task_09/Task9.R
+++ M335_Tasks/Case_Study_05/Class_Task_09/Task9.R
@@ -20,11 +20,38 @@ salaries <- Salaries %>%
 master <- Master %>%
   unite(playerName, nameGiven, nameLast, sep = " ") %>%
   select(playerName, playerID)
+inf <- inflation_adjust(2017)
 
-footballdata <- left_join(UTschools, CollegePlaying, by = "schoolID")
-footballdata <- left_join(footballdata, salaries, by = "playerID") 
-footballdata <- left_join(footballdata, master, by = "playerID") %>% 
+sportsdata <- left_join(UTschools, CollegePlaying, by = "schoolID")
+sportsdata <- left_join(sportsdata, salaries, by = "playerID") %>% 
   na.omit
+sportsdata <- left_join(sportsdata, master, by = "playerID") %>% 
+  na.omit %>%
+  mutate(salaryadjusted = salary * inf$adj_value)
+
+#Graphic
+
+#Because of time and total exhaustion I am looking at code from Mikal Bitter. 
+
+meanSal <-
+  sportsdata %>%
+  select(name_full, salaryadjusted) %>%
+  group_by(name_full) %>%
+  summarise(mean = mean(salaryadjusted))
+
+BYUPLot <- 
+  sportsdata %>% 
+  group_by(name_full) %>% 
+  ggplot(aes(x = name_full, y = salaryadjusted)) +
+    geom_boxplot(fill = "red") +
+    geom_point(data = meanSal, aes( x = name_full, y = mean ), size = 1.7, 
+      color = "blue") + 
+    scale_y_continuous( limits = c(0, 5000000) ) +
+    labs( x = "University", y = "Salary adj for Inflation (2017)", 
+      title = "Comparing Utah Salaries") +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggsave(filename = "Task9plot.png", plot = BYUPLot, width = 4, height = 4)
 
 
 
