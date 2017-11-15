7302ea1561b62929bd50cfd20cca4654572e0518
Stacy Wilkerson <wil15061@byui.edu>
2017-11-14 17:17:03 -0700

    updates

---
+++ M335_Tasks/Semester_Project/SemesterProj.R
@@ -0,0 +1,26 @@
+#Semester Project 
+
+library(tidyverse)
+library(magrittr)
+library(rvest)
+library(stringr)
+library(readr)
+
+waitlist_raw <- read_csv("WaitListData.csv", col_names = TRUE, col_types = cols(
+  X1 = col_character(),
+  X2 = col_character(),
+  X3 = col_character(),
+  X4 = col_character(),
+  X5 = col_character(),
+  `Waitlist # at end of day` = col_character(),
+  X7 = col_character()
+), skip = 1)
+
+waitlist_raw %>% 
+  filter(Status != "Withdrawn") %>%
+  ggplot(aes(x = Status)) +
+  geom_bar(aes(fill = `Waitlist Reason`)) +
+  facet_grid(~`Course Sec`)
+
+
+
